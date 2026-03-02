07b4e6b05028d02bb8a4aea0d8a85ce6b2b64d50
Linderoi <linderliberty@gmail.com>
2026-03-02 18:00:28 -0400

    Delete igarape-vivo-api directory

--- igarape-vivo-api/src/main/java/com/igarapevivo/repository/MonitoringRepository.java
+++
@@ -1,7 +0,0 @@
-package com.igarapevivo.repository;
-
-import com.igarapevivo.model.MonitoringData;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MonitoringRepository extends JpaRepository<MonitoringData, Long> {
-}
